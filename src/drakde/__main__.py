20e8e15bcf0b376ff77b75f1eb421494b2343076
Pavadol Yamsiri <pavyamsiri@gmail.com>
2026-07-21 20:58:58 +1000

    Set up benchmarking

--- src/drakde/__main__.py
+++ src/drakde/__main__.py
@@ -1,8 +1,9 @@
 import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib import colors as mplcolors
 
 from drakde._drakde import BivariateKDE
+import time
+
+show: bool = False
 
 if __name__ == "__main__":
     data = np.load("./data/data.npz")
@@ -21,6 +22,7 @@ if __name__ == "__main__":
     local_map = np.zeros_like(x_mesh)
     smooth_map = np.zeros_like(x_mesh)
 
+    start = time.perf_counter()
     for i in range(x_mesh.shape[0]):
         for j in range(x_mesh.shape[1]):
             current_x = x_mesh[i, j]
@@ -31,15 +33,24 @@ if __name__ == "__main__":
             smooth_map[i, j] = smoother.estimate_scalar(
                 current_x, current_y, smooth_scale
             )
-    fig = plt.figure()
-    axes = fig.add_subplot(111)
-
-    axes.pcolormesh(
-        x_mesh,
-        y_mesh,
-        local_map / smooth_map - 1.0,
-        cmap="seismic",
-        norm=mplcolors.Normalize(vmin=-0.05, vmax=0.05),
+    elapsed = time.perf_counter() - start
+    print(
+        f"Took {elapsed:2f} seconds to fill out a grid {x_mesh.shape[0]}x{x_mesh.shape[1]} or {x_mesh.shape[0] * x_mesh.shape[1]} points"
     )
-    plt.show()
-    plt.close()
+
+    if show:
+        from matplotlib import pyplot as plt
+        from matplotlib import colors as mplcolors
+
+        fig = plt.figure()
+        axes = fig.add_subplot(111)
+
+        axes.pcolormesh(
+            x_mesh,
+            y_mesh,
+            local_map / smooth_map - 1.0,
+            cmap="seismic",
+            norm=mplcolors.Normalize(vmin=-0.05, vmax=0.05),
+        )
+        plt.show()
+        plt.close()
